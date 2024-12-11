cb128283f44dd960511b6528a11aee77284e237a
Marcos-Andrade <andrademarcosferreira3@gmail.com>
2024-12-10 22:28:10 -0300

    Calculando o imc

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -22,11 +22,16 @@ class MainActivity : AppCompatActivity() {
 
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
-        btnCalcular.setOnClickListener {
-            val peso = edtPeso.text
-            val altura = edtAltura.text
 
-            println("Teste botão " + peso + " " + altura)
+
+        btnCalcular.setOnClickListener {
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val altura02 = altura * altura
+            val resultado = peso / altura
+
+            println("Seu índice IMC é: " + resultado)
 
         }
 
