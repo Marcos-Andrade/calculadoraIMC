d69b634b580e7f74d31338f66ce8b4f254872d5e
Marcos-Andrade <andrademarcosferreira3@gmail.com>
2024-12-16 20:26:22 -0300

    Mostrar mensagem para usuário preencher os campos vazios

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -25,15 +26,31 @@ class MainActivity : AppCompatActivity() {
 
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
 
-            val altura02 = altura * altura
-            val resultado = peso / altura
 
-            println("Seu índice IMC é: " + resultado)
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
+            if(pesoStr == "" || alturaStr == ""){
+                // Mostrar mensagem para o usuário
+
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val altura02 = altura * altura
+                val resultado = peso / altura02
+
+                println("Seu índice IMC é: " + resultado)
+            }
         }
-
     }
 }
